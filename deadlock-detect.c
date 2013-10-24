bafe2897491e36c1ab4fac8454ebbd97c9dc8b05
macdonad <macdonad@mail.gvsu.edu>
2013-10-24 17:01:16 -0400

    First

---
+++ deadlock-detect.c
@@ -0,0 +1,20 @@
+/*
+  Operating Systems
+
+  Prof. Wolffe
+
+  Program 2
+
+
+  Author: Doug MacDonald
+ */
+
+#include <stdio.h>
+
+
+int main(int argc, char* argv[])
+{
+  printf("Hello World\n");
+
+  return 0;
+}
