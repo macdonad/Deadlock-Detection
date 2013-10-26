c29a20cad4c3ef00c9ea419394c3187b0e7818d4
macdonad <macdonad@mail.gvsu.edu>
2013-10-26 09:18:31 -0400

    Read args

--- deadlock-detect.c
+++ deadlock-detect.c
@@ -10,11 +10,26 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
+#define debug 1
 
 int main(int argc, char* argv[])
 {
-  printf("Hello World\n");
+  int numberOfArgs = argc;
+  char* filename;
+  int processNumber;
+
+  if(numberOfArgs < 3)
+    {
+      printf("Not enough arguments specified.\nPlease provide filename and process number.\n");
+    }
+  else
+    {
+      filename = argv[1];
+      processNumber = atoi(argv[2]);
+      if(debug){printf("Filename: %s\nProcessNumber: %d\n", filename, processNumber);}
+    }
 
   return 0;
 }
