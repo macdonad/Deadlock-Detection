9bcdf8a314698654792baf6c7a0499d6152cfc6b
macdonad <macdonad@mail.gvsu.edu>
2013-10-28 18:47:08 -0400

    read file

--- deadlock-detect.c
+++ deadlock-detect.c
@@ -9,17 +9,30 @@
   Author: Doug MacDonald
  */
 
+//Include files
 #include <stdio.h>
 #include <stdlib.h>
 
+//Debug value
 #define debug 1
 
+
+//Prototypes
+void read_file(char*);
+void handle_line(char*);
+
+//Globals
+int blocked = 0;
+char* blockingProcess;
+int processNumber;
+char* processName;
+
+//Main
 int main(int argc, char* argv[])
 {
   int numberOfArgs = argc;
   char* filename;
-  int processNumber;
-  char* processName = argv[0];//Initialize
+  processName = argv[0];//Initialize
 
   if(numberOfArgs < 3)
     {
@@ -33,5 +46,47 @@ int main(int argc, char* argv[])
       if(debug){printf("Filename: %s\nProcessNumber: %d\nProcessName: %s\n", filename, processNumber, processName);}
     }
 
+  //read in file
+  read_file(filename);
+
+
   return 0;
+}
+
+void read_file(char* filename)
+{
+  int size = 1024, pos;
+  int c;
+  char* buffer = (char*)malloc(size);
+
+  FILE *file = fopen(filename, "r");
+  if(file)
+    {
+      do
+	{
+	  pos = 0;
+	  do
+	    {
+	      c = fgetc(file);
+	      if(c != EOF) buffer[pos++] = (char)c;
+	      if(pos >= size - 1)
+		{
+		  size *= 2;
+		  buffer = (char*)realloc(buffer, size);
+		}
+	    }
+	  while(c != EOF && c != '\n');
+	  buffer[pos] = 0;
+	  handle_line(buffer);
+	}
+      while(c != EOF);
+      fclose(file);
+    }
+  free(buffer);
+}
+
+//Takes line from text and checks 
+void handle_line(char* line)
+{
+  printf("%s", line);
 }
